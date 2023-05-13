4e8311be78f693f838b5582238998f67b905980f
zhebera <zhebera@yandex.by>
2023-05-13 19:57:12 +0300

    handling buttons on other activities from mainactivity

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,38 +16,33 @@ class MainActivity : AppCompatActivity() {
         val btnLibrary = findViewById<Button>(R.id.btn_library)
         val btnSettings = findViewById<Button>(R.id.btn_settings)
 
+        /** Обработка нажатия кнопок поиска и медиатеки двумя способами
         val btnSearchClickListener = object : View.OnClickListener{
             override fun onClick(v: View?) {
                 Toast.makeText(this@MainActivity, "Нажата кнопка поиска", Toast.LENGTH_SHORT).show()
             }
         }
 
-        val btnLibraryClickListener = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажата кнопка медиатеки", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        val btnSettingsClickListener = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажата кнопка настроек", Toast.LENGTH_SHORT).show()
-            }
-        }
-
         btnSearch.setOnClickListener(btnSearchClickListener)
-        btnLibrary.setOnClickListener(btnLibraryClickListener)
-        btnSettings.setOnClickListener(btnSettingsClickListener)
-
-        btnSearch.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажата кнопка поиска", Toast.LENGTH_SHORT).show()
-        }
 
         btnLibrary.setOnClickListener{
             Toast.makeText(this@MainActivity, "Нажата кнопка медиатеки", Toast.LENGTH_SHORT).show()
+        }*/
+
+        btnSearch.setOnClickListener {
+            val searchIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(searchIntent)
         }
 
-        btnSettings.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажата кнопка настроек", Toast.LENGTH_SHORT).show()
+        btnLibrary.setOnClickListener {
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
         }
+
+        btnSettings.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
     }
 }
