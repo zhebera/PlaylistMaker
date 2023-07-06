efa5d2aaaa3ca1c7a2937bf7a13cdb3982059c4a
zhebera <zhebera@yandex.by>
2023-07-06 16:27:08 +0300

    sprint12 Обработка смены темы приложения, добавлена верстка на историю поиска в activity_search.xml

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,6 +1,7 @@
 data class Track(
+    val trackId: String,         // Уникальный ID трека
     val trackName: String,       // Название композиции
     val artistName: String,      // Имя исполнителя
-    val trackTimeMillis: Long,        // Продолжительность трека
+    val trackTimeMillis: Long,   // Продолжительность трека
     val artworkUrl100: String    // Ссылка на изображение обложки
 )
